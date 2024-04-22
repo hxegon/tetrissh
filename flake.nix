cfd9172defe9399bc4b26742d072f0e4e3e4cbfa
Cooper LeBrun <cooperlebrun@gmail.com>
2024-04-22 12:10:29 -0400

    start with example server

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Multiplayer tetris over ssh";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      buildInputs = with pkgs; [ go gopls gotools go-tools just ];
+    in { devShell.${system} = pkgs.mkShell { inherit buildInputs; }; };
+}
